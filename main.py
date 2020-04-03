a352b0dbc5dd5c69b83640425a2fc1b005198e76
suletunahan2 <sule.tunahan@ogrenci.karatay.edu.tr>
2020-04-03 21:14:26 +0300

    main
    main oluşturuldu.

---
+++ main.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  3 21:11:35 2020
+
+@author: suletunahan2
+"""
+
+import pickle
+import numpy as np
+
+training_file = "./traffic-signs-data/train.p"
+validation_file= "./traffic-signs-data/valid.p"
+testing_file = "./traffic-signs-data/test.p"
+
+with open(training_file, mode='rb') as f:
+    train = pickle.load(f)
+with open(validation_file, mode='rb') as f:
+    valid = pickle.load(f)
+with open(testing_file, mode='rb') as f:
+    test = pickle.load(f)
