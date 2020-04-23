a79f20a5d764bc9564d5e01a50987cff5d80f9bf
suletunahan2 <sule.tunahan@ogrenci.karatay.edu.tr>
2020-04-23 20:28:26 +0300

    Data_load
    Data yüklendi.

--- main.py
+++ main.py
@@ -8,13 +8,20 @@ Created on Fri Apr  3 21:11:35 2020
 import pickle
 import numpy as np
 
-training_file = "./traffic-signs-data/train.p"
-validation_file= "./traffic-signs-data/valid.p"
-testing_file = "./traffic-signs-data/test.p"
+# Load pickled data
+import pickle
+import numpy as np
 
-with open(training_file, mode='rb') as f:
+training_file = '/content/drive/My Drive/Colab Notebooks/CNN/traffic-signs-data2.zip (Unzipped Files)/train.p'
+testing_file = '/content/drive/My Drive/Colab Notebooks/CNN/traffic-signs-data2.zip (Unzipped Files)/test.p'
+
+with open(training_file,
+          mode='rb') as f:  # rb:Opens a file for reading only in binary format. The file pointer is placed at the beginning of the file. This is the default mode.
     train = pickle.load(f)
-with open(validation_file, mode='rb') as f:
-    valid = pickle.load(f)
 with open(testing_file, mode='rb') as f:
     test = pickle.load(f)
+
+x_train, y_train = train['features'], train['labels']
+x_test, y_test = test['features'], test['labels']
+
+print('Reading data done!')
